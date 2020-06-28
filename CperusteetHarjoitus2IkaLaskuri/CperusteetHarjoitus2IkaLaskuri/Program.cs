0a89e8094cbb83d840ba59bfb16163db5b05e8ff
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-06-28 17:52:38 +0300

    Luotu arpakuutio-sovellus

--- CperusteetHarjoitus2IkaLaskuri/CperusteetHarjoitus2IkaLaskuri/Program.cs
+++ CperusteetHarjoitus2IkaLaskuri/CperusteetHarjoitus2IkaLaskuri/Program.cs
@@ -9,19 +9,38 @@ namespace CperusteetHarjoitus2IkaLaskuri
 {
 
     //Tehdään laskuri, joka laskee eletyn ajanhetken ja käyttäjän syöttämän ajan erotuksen.
+    //Laskuri antaa sukupuolen ja annetun syntymäpäivän perusteella eliniän ennusteen.
     class Program
     {
         static void Main(string[] args)
         {
-            Double aikaEro = 0; //Double-tyyppisessä muuttujassa on desimaalit mukana
-            string alkuAika = "";
-            DateTime syntymaAikaDT; //tähän muuttujaan tallennetaan käyttäjän syöttämä aika
+            Double aikaJaljella = 0; //Double-tyyppisessä muuttujassa on desimaalit mukana
+            int elinIanOdote = 0;
+            string alkuAika = "", sp="";
+            DateTime syntymaAikaDT, elinianOdotusDT; //tähän muuttujaan tallennetaan käyttäjän syöttämä aika
             DateTime tanaan = DateTime.Now; //tähän muuttujaan annetaan arvoksi kuluva ajanhetki. DateTime.Now sisältää kellonajan, DateTime.Today:ssä olisi vain päivämäärä
-            string formaatti = "yyyy.MM.dd HH:mm:sss"; //luodaan string-apumuuttuja -formaatti, jossa kerrotaan, missä muodossa päivämäärätieto tulee käyttäjältä.
+            string formaatti = "dd.MM.yyyy"; //luodaan string-apumuuttuja -formaatti, jossa kerrotaan, missä muodossa päivämäärätieto tulee käyttäjältä.
             CultureInfo kulttuuri = CultureInfo.InvariantCulture; //luodaan CultureInfo
 
-            //Pyydetään käyttäjää syöttämään aika
-            Console.WriteLine("Anna alkuaika muodossa VVVV.KK.PP HH:mm:ss");
+            Console.WriteLine("Kerro sukupuoli, M=Mies/N=Nainen");
+            sp = Console.ReadLine().ToUpper();
+            switch (sp) //mitä tutkitaan
+            {
+                case "M": //jos käyttäjä on syöttänyt M. Pitää olla lainausmerkit, koska on string eikä char.
+                    elinIanOdote = 78; //int-muuttuja elinIanOdote on 78
+                    break;
+
+                case "N": //jos käyttäjä on syöttänyt N
+                    elinIanOdote = 84;
+                    break;
+                default: //jos ei ole mikään yllä olevista
+                    Console.WriteLine("Virheellinen valinta!");
+                    elinIanOdote = 0; //ei välttämätön, koska lähtöarvoksi on syötetty jo nolla.
+                    break; 
+            }
+
+            //Pyydetään käyttäjää syöttämään syntymäaika
+            Console.WriteLine("Anna syntymäaika muodossa PP.KK.VVVV");
             alkuAika = Console.ReadLine(); //sijoitetaan string-muuttuja syntymaAikaan käyttäjän syöttämä syntymäaika. HUOM! Se on string-muodossa, kuten käyttäjän syöte aina.
             //Muutetaan yllä saatu string-tyyppinen tieto päivämääräksi:
             //Lisätään try-catch -käsittely
@@ -29,15 +48,18 @@ namespace CperusteetHarjoitus2IkaLaskuri
             {
                 //tätä ohjelma yrittää tehdä
                 syntymaAikaDT = DateTime.ParseExact(alkuAika, formaatti, kulttuuri); //Muunnetaan käyttäjän syöttämä päivämäärä oikeaan muotoon. ParseExact tarvitsee useamman parametrin, ensin muunnettava string-muuttuja, sitten formaatti, missä tieto tulee ja sitten CultureInfo
-                //Lasketaan päivämäärien erotus. Huom: tanaan-muuttuja on DateTime-tyyppinen, joten toisenkin käytettävän muuttujan pitää olla DateTime eli käytetään syntymaAikaDT-muuttujaa, johon yllä muunnettiin käyttäjän syöttämä tieto
-                aikaEro = tanaan.Subtract(syntymaAikaDT).TotalHours; //tallennetaan aikaEro muuttujan arvoksi tanaan-muuttujan ja syntymaAikaDT-muuttujan erotus
-                Console.WriteLine("Päivämäärien/aikojen erotus on " + aikaEro.ToString()); //tulostetaan konsolille laskennan tulos aikaEro muutettuna string-muotoon.
+                elinianOdotusDT = syntymaAikaDT.AddYears(elinIanOdote); //lisätään syntymäaikaan eliniän odote AddYears-metodilla, jolle annetaan parametriksi int elinIanOdote
+                //Lasketaan päivämäärien erotus. Huom: molempien muuttujien oltava DateTime-tyyppisiä jotta laskenta voidaan tehdä
+                aikaJaljella = elinianOdotusDT.Subtract(tanaan).TotalDays; //Vähennetään elinianOdotusDT-muuttujasta tänään eli DateTime.Now -arvo.
+                
             }
             catch (Exception ee) //Muodostettu exceptionista oma objekti. Jos ohjelma menee virheeseen, tapahtuu näin:
             {
                 Console.WriteLine("Ohjelma ei osannut laskea päivämääräerotusta. Tarkista pvm-formaatti!"); //eli tulee tämä itse muotoiltu virheilmoitus
                 Console.WriteLine(ee.Message); //Tulostetaan konsolille Exceptionin sisältämä message
+                aikaJaljella = 0;
             }
+            Console.WriteLine("Päivämäärien/aikojen erotus on " + aikaJaljella.ToString()); //tulostetaan konsolille laskennan tulos aikaEro muutettuna string-muotoon.
             Console.ReadLine();
         }
     }
---
+++ arpakuutio/arpakuutio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arpakuutio
+{
+    class Program
+    {
+        //Tehdään ohjelma, joka hakee randomluvun väliltä 1-3000
+        static void Main(string[] args)
+        {
+            Boolean lopeta = false;
+            Console.WriteLine("Tervetuloa random-generaattoriin!");
+            while (lopeta != true) //niin kauan kuin lopeta ei ole true, jatketaan. Sitten kun joku vastaa jotain, jonka vuoksi lopeta = true, lopetetaan.
+            {
+                Console.WriteLine("Haetaan seuraavat randomluvut");
+                randomLuvut(); //ohjelma kutsuu alirutiinia randomLuvut() -> siirtyy private static void randomLuvut -alirutiiniin
+                Console.WriteLine("Haetaanko luvut uudelleen? K=Kyllä/E=Ei");
+                if (Console.ReadLine().ToUpper() == "E") lopeta = true; //JOS konsolilta luettu käyttäjän syöttämä kirjain on isoksi muutettuna E, lopeta-boolean asetetaan trueksi jolloin silmukka päättyy (ei haeta enää uusia randomlukuja).
+            }
+        }
+
+        private static void randomLuvut()
+        {
+            int rluku = 0;
+            Random arpa = new Random(); //luodaan random-luokan olio
+            int i = 0; //luodaan i-laskurimuuttuja
+            //silmukka alkaa ja pyörii niin kauan kuin laskurimuuttuja on alle 100
+            while (i < 100)
+            {
+                rluku = arpa.Next(1, 3000); //haetaan uusi randomluku
+                Console.WriteLine("Silmukan kierroksella {1} randomluku on {0}", rluku.ToString(), (i + 1).ToString()); //tulostetaan randomluku
+                i++; //kasvatetaan i:n arvoa yhdellä
+            }
+        }
+    }
+}
