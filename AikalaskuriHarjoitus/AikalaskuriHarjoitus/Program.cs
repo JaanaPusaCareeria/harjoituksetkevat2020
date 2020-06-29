3c864d1796d8d5fa4ba3960886905e55f3ddec46
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-06-29 13:45:34 +0300

    Luotu aikalaskuriharjoitus

---
+++ AikalaskuriHarjoitus/AikalaskuriHarjoitus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AikalaskuriHarjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paiva1, paiva2;
+            DateTime paivienErotusDT;
+
+            Console.WriteLine("Anna päivämäärä nro 1");
+            paiva1 = Console.ReadLine();
+            DateTime paiva1DT = DateTime.Parse(paiva1);
+
+            Console.WriteLine("Anna toinen päivämäärä");
+            paiva2 = Console.ReadLine();
+            DateTime paiva2DT = DateTime.Parse(paiva2);
+
+            //paivienErotusDT = paiva1DT.Subtract(paiva2DT).TotalMinutes;
+
+        }
+    }
+}
