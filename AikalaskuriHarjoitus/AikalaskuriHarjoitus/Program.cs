47dab9fa197c4a21e08c78087244cb8a2293aa85
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-06-30 19:09:38 +0300

    Kokeiltu lisätä silmukka, toimii väärin. Lisätty päivämäärien erotuksen laskenta.

--- AikalaskuriHarjoitus/AikalaskuriHarjoitus/Program.cs
+++ AikalaskuriHarjoitus/AikalaskuriHarjoitus/Program.cs
@@ -8,20 +8,42 @@ namespace AikalaskuriHarjoitus
 {
     class Program
     {
+        //Tee konsolisovellus, joka kysyy käyttäjältä kaksi päivämäärää kellonaikoineen ja laskee niiden erotuksen minuuteissa.
+        //Tulos näytetään konsolilla ja rutiini laitetaan silmukan sisään, jotta käyttäjän on helppo syöttää uudet arvot ja laskea uusi erotus.
         static void Main(string[] args)
         {
             string paiva1, paiva2;
             DateTime paivienErotusDT;
+            Double paivienErotus = 0;
+            Boolean jatka = true;
 
-            Console.WriteLine("Anna päivämäärä nro 1");
-            paiva1 = Console.ReadLine();
-            DateTime paiva1DT = DateTime.Parse(paiva1);
+            while (jatka = true)
+            {
+                Console.WriteLine("Haluatko suorittaa laskennan uudelleen? K=Kyllä/E=Ei");
+                if (Console.ReadLine().ToUpper() == "K")
+                {
+                    jatka = true;
+                    Console.WriteLine("Anna päivämäärä nro 1");
+                    paiva1 = Console.ReadLine();
+                    DateTime paiva1DT = DateTime.Parse(paiva1);
 
-            Console.WriteLine("Anna toinen päivämäärä");
-            paiva2 = Console.ReadLine();
-            DateTime paiva2DT = DateTime.Parse(paiva2);
+                    Console.WriteLine("Anna toinen päivämäärä");
+                    paiva2 = Console.ReadLine();
+                    DateTime paiva2DT = DateTime.Parse(paiva2);
 
-            //paivienErotusDT = paiva1DT.Subtract(paiva2DT).TotalMinutes;
+                    paivienErotus = paiva1DT.Subtract(paiva2DT).TotalMinutes;
+
+                    Console.WriteLine("Päivämäärien erotus on {0} minuuttia.", paivienErotus.ToString());
+
+                    Console.ReadLine();
+
+                } else
+                {
+                    jatka = false;
+                }
+            }
+
+
 
         }
     }
