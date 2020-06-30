27b155259e565ae23ad5c92344c8a6d9b66dc3d7
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-06-30 18:55:16 +0300

    Lisätty merkkien määrän yhteenlasku ja tulostus konsoliin

--- Tiedonsyöttölomake/Tiedonsyöttölomake/Program.cs
+++ Tiedonsyöttölomake/Tiedonsyöttölomake/Program.cs
@@ -8,10 +8,13 @@ namespace Tiedonsyöttölomake
 {
     class Program
     {
+        //Tee sovellus, joka kysyy käyttäjältä etunimen, sukunimen, osoitteen, postinumeron, postitoimipaikan, puhelinnumeron ja sähköpostiosoitteen. Tiedot tallennetaan
+        //Console.ReadLinella kukin omaan muuttujaansa. Luvun jälkeen tiedot tulostetaan konsolille. Lopuksi selvitetään, montako merkkiä käyttäjä oli yhteensä syöttänyt 
+        //eri kenttiin. Blankoja ei lasketa. 
         static void Main(string[] args)
         {
             string etunimi, sukunimi, osoite, postinumero, postitoimipaikka, puhelinnumero, sahkoposti;
-
+            //Kysytään tiedot käyttäjältä ja tallennetaan muuttujiin
             Console.WriteLine("Anna etunimi");
             etunimi = Console.ReadLine();
             Console.WriteLine("Anna sukunimi");
@@ -26,11 +29,30 @@ namespace Tiedonsyöttölomake
             puhelinnumero = Console.ReadLine();
             Console.WriteLine("Anna sahkoposti");
             sahkoposti = Console.ReadLine();
-
+            //Tulostetaan tiedot konsolille
             Console.WriteLine("Tiedot ovat: " + "\r\n" + etunimi + " " + sukunimi + "\r\n" + osoite + "\r\n" + postinumero + " " + postitoimipaikka
                 + "\r\n" + puhelinnumero + "\r\n" + sahkoposti);
 
+            //Lasketaan kenttiin syötettyjen merkkien yhteissumma
+            etunimi = etunimi.Replace(" ", ""); //korvataan etunimi-muuttujan arvossa tyhjä mahdollinen blanko " " pois
+            int etunimilkm = etunimi.Length; //tallennetaan muuttujan merkkien lukumäärä etunimilkm-muuttujaan
+            sukunimi = sukunimi.Replace(" ", "");
+            int sukunimilkm = sukunimi.Length;
+            osoite = osoite.Replace(" ", "");
+            int osoitelkm = osoite.Length;
+            postinumero = postinumero.Replace(" ", "");
+            int postinumerolkm = postinumero.Length;
+            postitoimipaikka = postitoimipaikka.Replace(" ", "");
+            int postitoimipaikkalkm = postitoimipaikka.Length;
+            puhelinnumero = puhelinnumero.Replace(" ", "");
+            int puhelinnumerolkm = puhelinnumero.Length;
+            sahkoposti = sahkoposti.Replace(" ", "");
+            int sahkopostilkm = sahkoposti.Length;
+            
+            //Lasketaan muuttujien merkkimäärä yhteen uuteen muuttujaan
+            int merkkiensumma = etunimilkm + sukunimilkm + osoitelkm + postinumerolkm + postitoimipaikkalkm + puhelinnumerolkm + sahkopostilkm;
 
+            Console.WriteLine("Syöttämiesi merkkien summa on {0}", merkkiensumma.ToString()); //Tulostetaan konsolille yllä laskettu summa
             Console.ReadLine();
         }
     }
