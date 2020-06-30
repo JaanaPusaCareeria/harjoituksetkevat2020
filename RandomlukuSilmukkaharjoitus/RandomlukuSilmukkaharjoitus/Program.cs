d894755a86c2b8fc9a3fcb4f466821c54a6bdc46
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-06-30 18:41:05 +0300

    Kokeiltu tehdä silmukkaa, ei toimi

--- RandomlukuSilmukkaharjoitus/RandomlukuSilmukkaharjoitus/Program.cs
+++ RandomlukuSilmukkaharjoitus/RandomlukuSilmukkaharjoitus/Program.cs
@@ -8,17 +8,23 @@ namespace RandomlukuSilmukkaharjoitus
 {
     class Program
     {
+        //Tee sovellus, joka:
+        //asettaa satunnaisluvun (1,3001) muuttujaan
+        //toimii silmukassa
+        //silmukka etsii tätä satunnaislukua
+        //silmukassa lasketaan, montako kierrosta piti mennä, ennen kuin satunnaisluku löytyi
+        //silmukka pysähtyy, kun luku on löydetty ja ohjelma kertoo montako kierrosta meni
+        //EI TOIMI VIELÄ OIKEIN, TULEE AINA SAMAT LUVUT
         static void Main(string[] args)
         {
+             
+            Random random = new Random(); //luodaan random-olio
+            int rluku = random.Next(1, 3001); //annetaan int rluku-muuttujalle arvoksi seuraava random-olion arvo väliltä 1 - 3001
+            int i = 0, uusirluku = 0;
 
-            Random random = new Random();
-            int rluku = random.Next(1, 3001);
-            Random newrandom = new Random();
-            int uusirluku = newrandom.Next(1, 3001);
-            int i = 0;
-
-            while (rluku != uusirluku)
+            while (rluku != uusirluku) 
             {
+                Random newrandom = new Random();
                 uusirluku = newrandom.Next(1, 3001);
                 Console.WriteLine("Kierroksella {0} luku oli {1}", (i + 1).ToString(), uusirluku.ToString());
                 i++;
