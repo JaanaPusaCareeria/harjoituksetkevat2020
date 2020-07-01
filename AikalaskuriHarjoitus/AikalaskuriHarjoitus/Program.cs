f23ddcb2b126696257a7ab0708c4bccf3e72ca4c
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-07-01 18:22:53 +0300

    Muutettu varsinainen laskenta alirutiiniksi. Lisätty while-silmukka joka pyörittää alirutiinia. Lisätty laskentarutiiniin try-catch-käsittely.

--- AikalaskuriHarjoitus/AikalaskuriHarjoitus/Program.cs
+++ AikalaskuriHarjoitus/AikalaskuriHarjoitus/Program.cs
@@ -10,42 +10,51 @@ namespace AikalaskuriHarjoitus
     {
         //Tee konsolisovellus, joka kysyy käyttäjältä kaksi päivämäärää kellonaikoineen ja laskee niiden erotuksen minuuteissa.
         //Tulos näytetään konsolilla ja rutiini laitetaan silmukan sisään, jotta käyttäjän on helppo syöttää uudet arvot ja laskea uusi erotus.
-        //KESKEN
+        //Muutettu laskenta-rutiini omaksi alirutiinikseen
+        //Lisätty while-silmukka ja sinne laskennan kutsu sekä try-catch virheen käsittely laskentarutiiniin
+        //Päivämääräformaatti? 
         static void Main(string[] args)
         {
-            string paiva1, paiva2;
-            DateTime paivienErotusDT;
-            Double paivienErotus = 0;
-            Boolean jatka = true;
+            Laskenta(); //kutsuu laskenta-alirutiinia, joka käydään läpi kerran ennen siirtymistä while-silmukkaan
+            Boolean jatka = true; //boolean-muuttuja jatka arvo asetetaan tässä trueksi
 
-            while (jatka = true)
+            while (jatka == true) //while-silmukka pyörii, niin kauan kun jatka-muuttujan arvo on true
             {
-                Console.WriteLine("Haluatko suorittaa laskennan uudelleen? K=Kyllä/E=Ei");
-                if (Console.ReadLine().ToUpper() == "K")
+                Console.WriteLine("Haluatko suorittaa laskennan uudelleen? K=Kylläl/E=Ei"); //kysytään käyttäjältä, haluaako hän jatkaa
+                if (Console.ReadLine().ToUpper() == "K") //jos käyttäjän syöttämä tieto konsolilta luettuna on isoksi muutettuna "K"
                 {
-                    jatka = true;
-                    Console.WriteLine("Anna päivämäärä nro 1");
-                    paiva1 = Console.ReadLine();
-                    DateTime paiva1DT = DateTime.Parse(paiva1);
-
-                    Console.WriteLine("Anna toinen päivämäärä");
-                    paiva2 = Console.ReadLine();
-                    DateTime paiva2DT = DateTime.Parse(paiva2);
-
-                    paivienErotus = paiva1DT.Subtract(paiva2DT).TotalMinutes;
-
-                    Console.WriteLine("Päivämäärien erotus on {0} minuuttia.", paivienErotus.ToString());
-
-                    Console.ReadLine();
-
-                } else
+                    Laskenta(); //kutsutaan taas Laskenta-alirutiinia
+                    jatka = true; //ja asetetaan jatka-muuttujan arvoksi taas true
+                } else //jos käyttäjän syöttämä arvo on jotakin muuta kuin "K"
                 {
-                    jatka = false;
-                }
+                    jatka = false; //asetetaan jatka-muuttujan arvoksi false, silmukka päättyy ja ohjelman suoritus päättyy
+                } 
+            }
+        }
+
+        private static void Laskenta()
+        {
+            string paiva1, paiva2;
+            Double paivienErotus = 0;
+
+            Console.WriteLine("Anna ensimmäinen päivämäärä muodossa dd.MM.yyyy"); //kysytään käyttäjältä päivämäärä kellonaikoineen
+            paiva1 = Console.ReadLine(); //asetetaan konsolilta luettu arvo paiva1 muuttujaan
+            try
+            {
+                DateTime paiva1DT = DateTime.Parse(paiva1); //muutetaan konsolilta saatu tieto DateTime.Parsella DateTime-tyyppiseksi paiva1DT-muuttujaksi
+                Console.WriteLine("Anna toinen päivämäärä muodossa dd.MM.yyyy"); //perusasetus ei vaadi kellonajalle muuta formaattia, hyväksyy sen tälläisenään.
+                paiva2 = Console.ReadLine();
+                DateTime paiva2DT = DateTime.Parse(paiva2);
+                paivienErotus = paiva1DT.Subtract(paiva2DT).TotalMinutes; //tallennetaan paiva1DT:n ja paiva2DT:n erotus double-tyyppiseen muuttujaan paivienErotus.
+                Console.WriteLine("Päivämäärien erotus on {0} minuuttia.", paivienErotus.ToString()); //tulostetaan laskettu erotus konsolille
+        }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Tarkista syöttämäsi arvo");
             }
 
 
-
-        }
+}
     }
 }
