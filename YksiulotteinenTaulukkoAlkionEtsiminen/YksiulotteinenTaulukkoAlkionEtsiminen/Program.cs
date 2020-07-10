1ece1fca45744d8acd98e877eb80f640790dfb2e
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-07-10 13:32:32 +0300

    Lisätty silmukkakäsittely koko rutiinille

--- YksiulotteinenTaulukkoAlkionEtsiminen/YksiulotteinenTaulukkoAlkionEtsiminen/Program.cs
+++ YksiulotteinenTaulukkoAlkionEtsiminen/YksiulotteinenTaulukkoAlkionEtsiminen/Program.cs
@@ -14,33 +14,35 @@ namespace YksiulotteinenTaulukkoAlkionEtsiminen
         //Jos käyttäjän antama luku löytyy, anna onnitteluilmoitus. Jos ei löydy, anna ilmoitus "lukua ei löydy".
         //Muuten onnistui, mutta nyt konsoli tulostaa JOKA läpikäynnillä ilmoituksen..
         //Korjattu silmukan tulostus muistiinpanojen avulla
+        //Lisätty oma silmukkakäsittely rutiinille
         static void Main(string[] args)
         {
             string arvaus = "";
             int arvausLuku = 0, laskuri = 0;
-            bool löytyi;
+            bool löytyi, jatka = true; //luodaan boolean löytyi-muuttuja ilman alkuarvoa ja jatka-muuttuja alkuarvolla true(jotta päästään silmukkaan)
             int[] taulukko1 = new int[] { 2000, 1, 3, 5, 16, 77, 101, 44, 1324, 3234 }; //luodaan taulukko ja annetaan sille alkiot
-
-            Console.Write("Anna luku: "); //kysytään käyttäjältä luku
-            arvaus = Console.ReadLine(); //tallennetaan luku arvaus-muuttujaan
-
-            if (!int.TryParse(arvaus, out arvausLuku) == true)
+            while (jatka == true) //niin kauan, kuin jatka-muuttujan arvo on true
             {
-                Console.WriteLine("Syöttämäsi tieto ei ollut numeerinen");
-            }
-            löytyi = false;
-
-            for (int i = 0; i < taulukko1.Length; i++) 
-            {
-                if (arvausLuku == taulukko1[i])
+                Console.Write("Anna luku (X lopettaa): "); //kysytään käyttäjältä luku
+                arvaus = Console.ReadLine(); //tallennetaan luku arvaus-muuttujaan
+                if (arvaus.ToUpper() == "X") //jos arvaus muutettuna isoksi on X
                 {
-                    Console.WriteLine("Onneksi olkoon, luku {0} vastaa taulukon alkiota {1}!", arvausLuku.ToString(), i.ToString());
-                    löytyi = true;
+                    jatka = false; //jatka-muuttuja asetetaan falseksi
+                    break;//poistutaan silmukasta ja lopetetaan ohjelma
                 }
-                laskuri = i + 1;
+                löytyi = false; //löytyi-muuttuja asetetaan falseksi
+                arvausLuku = int.Parse(arvaus); //muutetaan arvaus int-tyyppiseksi arvausLuku-muuttujaksi
+                for (int i = 0; i < taulukko1.Length; i++) //käydään koko taulukko läpi
+                {
+                    if (arvausLuku == taulukko1[i]) //jos arvausLuku vastaa sen kierroksen taulukon alkiota
+                    {
+                        Console.WriteLine("Onneksi olkoon, luku {0} vastaa taulukon alkiota {1}!", arvausLuku.ToString(), i.ToString()); //tulostetaan konsolille onnittelu
+                        löytyi = true; //löytyi muuttuja asetetaan falseksi
+                    }
+                    laskuri = i + 1; //kasvatetaan laskuri-muuttujan arvoa yhdellä
+                }
+                if (löytyi == false) Console.WriteLine("Kävin läpi {0} lukua, mutta en löytänyt lukua {1}!", laskuri.ToString(), arvausLuku.ToString());//jos löytyi-muuttujan arvo on false eli arvausLuku ei vastannut muuttujaa, tulostetaan konsolille tämä
             }
-
-            if (löytyi == false) Console.WriteLine("Kävin läpi {0} lukua, mutta en löytänyt lukua {1}!", laskuri.ToString(), arvausLuku.ToString());
             Console.ReadLine();
         }
     }
