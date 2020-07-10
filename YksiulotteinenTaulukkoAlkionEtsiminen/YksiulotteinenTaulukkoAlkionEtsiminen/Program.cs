7919df93fba943e0d217c3314f9930bad789eea6
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-07-10 13:08:31 +0300

    Korjattu for-silmukan toiminta, lisätty alkuun TryParse tarkistus, onko syötetty tieto luku

--- YksiulotteinenTaulukkoAlkionEtsiminen/YksiulotteinenTaulukkoAlkionEtsiminen/Program.cs
+++ YksiulotteinenTaulukkoAlkionEtsiminen/YksiulotteinenTaulukkoAlkionEtsiminen/Program.cs
@@ -13,28 +13,34 @@ namespace YksiulotteinenTaulukkoAlkionEtsiminen
         //int[] taulukko1 = new int[] {2000, 1, 3, 5, 16, 77, 101, 44, 1324, 3234}. Kysy käyttäjältä lukua Console.ReadLinella. Käy taulukko läpi silmukassa
         //Jos käyttäjän antama luku löytyy, anna onnitteluilmoitus. Jos ei löydy, anna ilmoitus "lukua ei löydy".
         //Muuten onnistui, mutta nyt konsoli tulostaa JOKA läpikäynnillä ilmoituksen..
+        //Korjattu silmukan tulostus muistiinpanojen avulla
         static void Main(string[] args)
         {
             string arvaus = "";
-            Boolean jatka = true;
-            int arvausLuku = 0;
+            int arvausLuku = 0, laskuri = 0;
+            bool löytyi;
             int[] taulukko1 = new int[] { 2000, 1, 3, 5, 16, 77, 101, 44, 1324, 3234 }; //luodaan taulukko ja annetaan sille alkiot
 
             Console.Write("Anna luku: "); //kysytään käyttäjältä luku
             arvaus = Console.ReadLine(); //tallennetaan luku arvaus-muuttujaan
-            arvausLuku = int.Parse(arvaus);
 
-            for (int i = 0; i < taulukko1.Length; i++)
+            if (!int.TryParse(arvaus, out arvausLuku) == true)
+            {
+                Console.WriteLine("Syöttämäsi tieto ei ollut numeerinen");
+            }
+            löytyi = false;
+
+            for (int i = 0; i < taulukko1.Length; i++) 
             {
                 if (arvausLuku == taulukko1[i])
                 {
                     Console.WriteLine("Onneksi olkoon, luku {0} vastaa taulukon alkiota {1}!", arvausLuku.ToString(), i.ToString());
-                } else
-                {
-                    Console.WriteLine("Valitettavasti lukua ei löytynyt");
+                    löytyi = true;
                 }
+                laskuri = i + 1;
             }
 
+            if (löytyi == false) Console.WriteLine("Kävin läpi {0} lukua, mutta en löytänyt lukua {1}!", laskuri.ToString(), arvausLuku.ToString());
             Console.ReadLine();
         }
     }
