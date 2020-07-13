41e27610e0b294bbe7fcdd107ffbf273cfd5e052
Jaana Pusa <jaana.pusa@student.careeria.fi>
2020-07-13 15:04:25 +0300

    Lisätty listan tulostus ja arvausmäärien kertominen

--- ArvausPeliHarjoitus/ArvausPeliHarjoitus/Program.cs
+++ ArvausPeliHarjoitus/ArvausPeliHarjoitus/Program.cs
@@ -13,11 +13,11 @@ namespace ArvausPeliHarjoitus
        //Mikäli käyttäjä arvaa liian suuren luvun, ohjelma sanoo "luku on pienempi kuin arvaus" ja mikäli käyttäjä arvaa liian pienen luvun, ohjelma
        //sanoo "luku on suurempi kuin arvaus". Arvaukset tallennetaan listalle. Lopuksi, kun arvaus osuu oikeaan, käyttäjää onnitellaan ja kerrotaan montako arvausta 
        //tarvittiin sekä tulostetaan listalla olevat arvaukset.
-       //Välitallennus, saatu muuten toimimaan paitsi listalle tallennus
+       //Lisätty listan tulostus ja arvausmäärien kertominen.
         static void Main(string[] args)
         {
             string arvaus;
-            int arvausLuku, rluku;
+            int arvausLuku, rluku, laskuri = 0;
             bool jatka = true; //asetetaan boolean jatka-muuttujan lähtöarvoksi true, jotta päästään while-silmukkaan
             Random luku = new Random(); //luodaan uusi random-muuttuja
             rluku = luku.Next(1, 101); //haetaan int rluku-muuttujalle seuraava random-arvo välilltä 1-100.
@@ -27,27 +27,28 @@ namespace ArvausPeliHarjoitus
 
             while (jatka == true) //niin kauan kuin jatka-muuttujan arvo on true, silmukka pyörii
             {
-                Console.WriteLine("Arvaa luku: "); //kysytään käyttäjältä luku
+                Console.Write("Arvaa, minkä luvun peli arpoi: "); //kysytään käyttäjältä luku
                 arvaus = Console.ReadLine(); //tallennetaan saatu tieto arvaus-muuttujaan
                 try
                 {
                     arvausLuku = int.Parse(arvaus); //tallennetaan arvausLuku-muuttujaan int.Parsen avulla arvaus-muuttujan arvo
-                    //arvausLista.Add(arvausLuku); //lisätään arvausLuku arvausListalle alkioksi
-                    Console.WriteLine(arvausLuku);
-                    if (arvausLuku == rluku)
+                    arvausLista.Add(arvausLuku); //lisätään arvausLuku arvausListalle alkioksi
+                    if (arvausLuku < rluku)
                     {
-                        Console.WriteLine("Onneksi olkoon, arvasit oikein!");
-                        jatka = false;
-                        //for (int i = 0; i < arvausLista.Count; i++)
-                        //{
-                        //    Console.WriteLine("Arvaus numero {0} oli {1}", (i + 1).ToString(), arvausLuku.ToString()); ;
-                        //}
+                        Console.WriteLine("Arvauksesi oli pienempi kuin luku");
                     } else if (arvausLuku > rluku)
                     {
                         Console.WriteLine("Arvauksesi oli suurempi kuin luku");
                     } else
                     {
-                        Console.Write("Arvauksesi oli pienempi kuin luku");
+                        Console.WriteLine("Onneksi olkoon, arvasit oikein!");
+                        for (int i = 0; i < arvausLista.Count; i++)
+                        {
+                            Console.WriteLine("Arvaus numero {0} oli {1}", (i + 1).ToString(), arvausLista[i]);
+                            laskuri++;
+                        }
+                        Console.WriteLine("Onnistuminen vaati {0} yritystä.", laskuri.ToString());
+                        jatka = false;
                     }
                 }
                 catch (Exception) //jos arvaus-muuttujan arvoa ei voida muuttaa int.Parsella ts se ei ole numeerinen
